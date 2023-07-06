0b46ea968186f575986765a64e40cbde513151b5
Nato Boram <nato.boram@gmail.com>
2023-07-06 16:19:31 -0400

    Reproduce bug
    https://github.com/sveltejs/kit/issues/10332

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapterAuto from '@sveltejs/adapter-auto';
+import adapterNode from '@sveltejs/adapter-node';
+import adapterStatic from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { loadEnv } from 'vite';
+
+/** @type {{BUILD_ADAPTER: "static" | "node" | undefined, BUILD_BASE: string | undefined}} */
+const env = loadEnv(process.env.NODE_ENV ?? 'development', process.cwd(), 'BUILD_');
+
+/** @returns {import('@sveltejs/kit').Adapter} */
+function adapter() {
+	if (env.BUILD_ADAPTER === 'node') return adapterNode();
+	else if (env.BUILD_ADAPTER === 'static') return adapterStatic({ fallback: '404.html' });
+	else if (process.env.GITHUB_ACTIONS) return adapterStatic({ fallback: '404.html' });
+	return adapterAuto();
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
